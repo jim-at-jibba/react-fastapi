3cd9580debd665bd9138be525d93a6614446fd50
James Best <jim@justjibba.net>
2022-07-30 05:15:51 +0100

    Add models

---
+++ backend/models.py
@@ -0,0 +1,32 @@
+import database as database
+import datatime as dt
+import passlib.hash as hash
+import sqlalchemy as sql
+import sqlalchemy.orm as orm
+
+
+class User(database.Base):
+    __tablename__ = "users"
+    id = sql.Column(sql.Integer, primary_key=True, index=True)
+    email = sql.Column(sql.String, unique=True, index=True)
+    hashed_password = sql.Column(sql.String)
+
+    leads = orm.relationship("Lead", back_populates="owner")
+
+    def verify_password(self, password: str):
+        return hash.bcrypt.verify(password, self.hashed_password)
+
+
+class Lead(database.Base):
+    __tablename__ = "leads"
+    id = sql.Column(sql.Integer, primary_key=True, index=True)
+    owner_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"))
+    first_name = sql.Column(sql.String, index=True)
+    last_name = sql.Column(sql.String, index=True)
+    email = sql.Column(sql.String, index=True)
+    company = sql.Column(sql.String, index=True, default="")
+    note = sql.Column(sql.String, default="")
+    date_created = sql.Column(sql.DateTime, default=dt.datetime.utcnow)
+    date_last_updated = sql.Column(sql.DateTime, default=dt.datetime.utcnow)
+
+    owner = orm.relationship("User", back_populates="leads")
